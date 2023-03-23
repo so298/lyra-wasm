c75e4e57f3f51ded935bac9e7e6b97cc3729ce73
Sosuke Hosokawa <so298go@gmail.com>
2023-03-23 18:16:45 +0900

    Add EMSCRIPTEN_KEEPALIVE to header file

--- wasm/lyra_c_api.h
+++ wasm/lyra_c_api.h
@@ -17,32 +17,45 @@ struct lyra_decoder;
 struct lyra_vector_u8;
 struct lyra_vector_s16;
 
+EMSCRIPTEN_KEEPALIVE
 lyra_encoder* lyra_encoder_create(int sample_rate_hz,
                                   int num_channels,
                                   int bitrate,
                                   bool enable_dtx,
                                   const char* model_path);
+EMSCRIPTEN_KEEPALIVE
 lyra_vector_u8* lyra_encoder_encode(lyra_encoder* encoder,
                                     const int16_t* audio,
                                     size_t length);
+EMSCRIPTEN_KEEPALIVE
 void lyra_encoder_destroy(lyra_encoder* encoder);
 
+EMSCRIPTEN_KEEPALIVE
 lyra_decoder* lyra_decoder_create(int sample_rate_hz,
                                   int num_channels,
                                   const char* model_path);
+EMSCRIPTEN_KEEPALIVE
 bool lyra_decoder_set_encoded_packet(lyra_decoder* decoder,
                                      const uint8_t* encoded,
                                      size_t length);
+EMSCRIPTEN_KEEPALIVE
 lyra_vector_s16* lyra_decoder_decode_samples(lyra_decoder* decoder,
                                              int num_samples);
+EMSCRIPTEN_KEEPALIVE
 void lyra_decoder_destroy(lyra_decoder* decoder);
 
+EMSCRIPTEN_KEEPALIVE
 size_t lyra_vector_u8_get_size(lyra_vector_u8* v);
+EMSCRIPTEN_KEEPALIVE
 uint8_t* lyra_vector_u8_get_data(lyra_vector_u8* v);
+EMSCRIPTEN_KEEPALIVE
 void lyra_vector_u8_destroy(lyra_vector_u8* v);
 
+EMSCRIPTEN_KEEPALIVE
 size_t lyra_vector_s16_get_size(lyra_vector_s16* v);
+EMSCRIPTEN_KEEPALIVE
 int16_t* lyra_vector_s16_get_data(lyra_vector_s16* v);
+EMSCRIPTEN_KEEPALIVE
 void lyra_vector_s16_destroy(lyra_vector_s16* v);
 }
 
